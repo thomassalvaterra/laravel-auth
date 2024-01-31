319c57950349b3b319c7ee9095a0d32a2a1557f6
Thomas66 <thomassalvaterra2001@gmail.com>
2024-01-31 17:13:40 +0100

    new table name

--- database/migrations/2024_01_30_142422_create_games_table.php
+++ database/migrations/2024_01_30_142422_create_games_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('oggetto_portafoglios', function (Blueprint $table) {
+        Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->string('title', 100);
             $table->string('description');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('oggetto_portafoglios');
+        Schema::dropIfExists('games');
     }
 };
