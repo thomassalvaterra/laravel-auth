0da80427c6f136e84a8a00bc323fef9166b4eac4
Thomas66 <thomassalvaterra2001@gmail.com>
2024-01-31 12:24:55 +0100

    feat(dashboard): create controller and view for admin users

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Admin\DashboardController; //<---- Import del controller precedentemente creato!
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('admin.dashboard');
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth'])
+    ->prefix('admin') //definisce il prefisso "admin/" per le rotte di questo gruppo
+    ->name('admin.') //definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
+    ->group(function () {
+
+        //Siamo nel gruppo quindi:
+        // - il percorso "/" diventa "admin/"
+        // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    });
+
+require __DIR__ . '/auth.php';
